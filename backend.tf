b7a194fbbe871663396927dd0b7a74ded577b379
Anton Sv <svyatoj@gmail.com>
2022-06-14 14:01:28 +0000

    Fix ansible error

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     region  = "us-east-1"
     profile = "default"
     key     = "terraformstatefile"
-    bucket  = "terraformstatebucket2203"
+    bucket  = "terraformstatebucket2204"
   }
 }
